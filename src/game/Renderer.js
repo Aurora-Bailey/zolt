ffd0e9c9cd3d579e9e26cefce3d42aa504ba898c
Brad Thompson <bradleyglind@gmail.com>
2016-11-14 02:26:15 -0700

    Split out rendering to a separate module

---
+++ src/game/Renderer.js
@@ -0,0 +1,96 @@
+/* Modules */
+var THREE = require('three')
+import Data from '../modules/Data'
+
+var renderWidth = 1920
+var renderHeight = 1080
+var scene, camera, renderer
+
+scene = new THREE.Scene()
+camera = new THREE.PerspectiveCamera(30, renderWidth / renderHeight, 1, 10000)
+camera.up = new THREE.Vector3(0, 0, 1)
+camera.position.z = 3000
+camera.position.y = -1250
+
+renderer = new THREE.WebGLRenderer()
+renderer.setSize(renderWidth, renderHeight)
+renderer.setClearColor(0x000000)
+
+var material = []
+var geometry = []
+var mesh = []
+var light = []
+
+geometry[0] = new THREE.BoxGeometry(200, 200, 200)
+material[0] = new THREE.MeshPhongMaterial({color: 0xf6546a})
+
+for (let i = 0; i < 1000; i++) {
+  let size = 10000
+  mesh[i] = new THREE.Mesh(geometry[0], material[0])
+  mesh[i].position.set(Math.floor(Math.random() * size) - (size / 2), Math.floor(Math.random() * size) - (size / 2), 100)
+  scene.add(mesh[i])
+}
+
+light[0] = new THREE.PointLight(0xffffff, 1)
+light[0].position.set(0, 0, 400)
+scene.add(light[0])
+
+var playerSphere = new THREE.SphereGeometry(100, 32, 32)
+var playerMat = new THREE.MeshPhongMaterial({color: 0xffffff})
+var player = new THREE.Mesh(playerSphere, playerMat)
+player.position.set(0, 0, 100)
+scene.add(player)
+
+var floorPlane = new THREE.PlaneGeometry(10000, 10000)
+var floorMat = new THREE.MeshPhongMaterial({color: 0x555555, side: THREE.DoubleSide})
+var floor = new THREE.Mesh(floorPlane, floorMat)
+scene.add(floor)
+
+let alpha = 0
+var animate = () => {
+  window.requestAnimationFrame(animate)
+  /* Rotate camera around camera point
+   camera.rotation.z = Data.input.rotation
+   camera.rotation.y = Math.sin(Data.input.rotation) * (Math.PI / 8)
+   camera.rotation.x = Math.cos(Data.input.rotation) * (Math.PI / 8)
+   */
+
+  /* User Input */
+  if (Data.input.w) camera.position.z += 5
+  if (Data.input.s) camera.position.z -= 5
+  if (Data.input.right) {
+    player.position.x -= 5 * Math.sin(Data.input.rotation)
+    player.position.y += 5 * Math.cos(Data.input.rotation)
+  }
+  if (Data.input.left) {
+    player.position.x += 5 * Math.sin(Data.input.rotation)
+    player.position.y -= 5 * Math.cos(Data.input.rotation)
+  }
+  if (Data.input.up) {
+    player.position.y -= 5 * Math.sin(Data.input.rotation)
+    player.position.x -= 5 * Math.cos(Data.input.rotation)
+  }
+  if (Data.input.down) {
+    player.position.y += 5 * Math.sin(Data.input.rotation)
+    player.position.x += 5 * Math.cos(Data.input.rotation)
+  }
+
+  /* Rotate camera around player */
+  camera.position.x = player.position.x + (Math.cos(Data.input.rotation) * 1250)
+  camera.position.y = player.position.y + (Math.sin(Data.input.rotation) * 1250)
+  camera.lookAt(player.position)
+
+  /* Dytamic light */
+  light[0].position.x = Math.cos(alpha) * 500 + player.position.x
+  light[0].position.y = Math.sin(alpha) * 500 + player.position.y
+
+  renderer.render(scene, camera)
+  alpha += 0.01
+}
+animate()
+
+export default {
+  GetCanvas: function () {
+    return renderer.domElement
+  }
+}
--- src/pages/Three.vue
+++ src/pages/Three.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="three" v-bind:class="{inactive: !$root.input.mouseLock}">
     <div class="click_to_continue" v-on:click="LockMouse()"><div class="center">Click here to continue...</div></div>
-    <div id="three_demo"></div>
+    <div id="three_canvas"></div>
   </div>
 </template>
 
 <script>
   /* Modules */
-  var THREE = require('three')
+  import Renderer from '../game/Renderer'
   import MouseLock from '../modules/MouseLock'
 
   export default {
@@ -18,93 +18,7 @@
       }
     },
     mounted: function () {
-      var renderWidth = 1920
-      var renderHeight = 1080
-      var scene, camera, renderer
-
-      scene = new THREE.Scene()
-      camera = new THREE.PerspectiveCamera(30, renderWidth / renderHeight, 1, 10000)
-      camera.up = new THREE.Vector3(0, 0, 1)
-      camera.position.z = 3000
-      camera.position.y = -1250
-
-      renderer = new THREE.WebGLRenderer()
-      renderer.setSize(renderWidth, renderHeight)
-      renderer.setClearColor(0x000000)
-      document.getElementById('three_demo').appendChild(renderer.domElement)
-
-      var material = []
-      var geometry = []
-      var mesh = []
-      var light = []
-
-      geometry[0] = new THREE.BoxGeometry(200, 200, 200)
-      material[0] = new THREE.MeshPhongMaterial({color: 0xf6546a})
-
-      for (let i = 0; i < 1000; i++) {
-        let size = 10000
-        mesh[i] = new THREE.Mesh(geometry[0], material[0])
-        mesh[i].position.set(Math.floor(Math.random() * size) - (size / 2), Math.floor(Math.random() * size) - (size / 2), 100)
-        scene.add(mesh[i])
-      }
-
-      light[0] = new THREE.PointLight(0xffffff, 1)
-      light[0].position.set(0, 0, 400)
-      scene.add(light[0])
-
-      var playerSphere = new THREE.SphereGeometry(100, 32, 32)
-      var playerMat = new THREE.MeshPhongMaterial({color: 0xffffff})
-      var player = new THREE.Mesh(playerSphere, playerMat)
-      player.position.set(0, 0, 100)
-      scene.add(player)
-
-      var floorPlane = new THREE.PlaneGeometry(10000, 10000)
-      var floorMat = new THREE.MeshPhongMaterial({color: 0x555555, side: THREE.DoubleSide})
-      var floor = new THREE.Mesh(floorPlane, floorMat)
-      scene.add(floor)
-
-      let alpha = 0
-      var animate = () => {
-        window.requestAnimationFrame(animate)
-        /* Rotate camera around camera point
-        camera.rotation.z = this.$root.input.rotation
-        camera.rotation.y = Math.sin(this.$root.input.rotation) * (Math.PI / 8)
-        camera.rotation.x = Math.cos(this.$root.input.rotation) * (Math.PI / 8)
-        */
-
-        /* User Input */
-        if (this.$root.input.w) camera.position.z += 5
-        if (this.$root.input.s) camera.position.z -= 5
-        if (this.$root.input.right) {
-          player.position.x -= 5 * Math.sin(this.$root.input.rotation)
-          player.position.y += 5 * Math.cos(this.$root.input.rotation)
-        }
-        if (this.$root.input.left) {
-          player.position.x += 5 * Math.sin(this.$root.input.rotation)
-          player.position.y -= 5 * Math.cos(this.$root.input.rotation)
-        }
-        if (this.$root.input.up) {
-          player.position.y -= 5 * Math.sin(this.$root.input.rotation)
-          player.position.x -= 5 * Math.cos(this.$root.input.rotation)
-        }
-        if (this.$root.input.down) {
-          player.position.y += 5 * Math.sin(this.$root.input.rotation)
-          player.position.x += 5 * Math.cos(this.$root.input.rotation)
-        }
-
-        /* Rotate camera around player */
-        camera.position.x = player.position.x + (Math.cos(this.$root.input.rotation) * 1250)
-        camera.position.y = player.position.y + (Math.sin(this.$root.input.rotation) * 1250)
-        camera.lookAt(player.position)
-
-        /* Dytamic light */
-        light[0].position.x = Math.cos(alpha) * 500 + player.position.x
-        light[0].position.y = Math.sin(alpha) * 500 + player.position.y
-
-        renderer.render(scene, camera)
-        alpha += 0.01
-      }
-      animate()
+      document.getElementById('three_canvas').appendChild(Renderer.GetCanvas())
     }
   }
 </script>
@@ -116,7 +30,7 @@
   @import "../sass/mixins";
 
   .three {
-    #three_demo {
+    #three_canvas {
       // render window styles
     }
     .click_to_continue {
@@ -126,7 +40,7 @@
 
   /* Pointer is not locked */
   .inactive {
-    #three_demo {
+    #three_canvas {
       filter: blur(5px) sepia(100%);
     }
     .click_to_continue {
