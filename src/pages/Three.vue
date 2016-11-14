c04dcb9b46c21e60128696656847c1003dc032e3
Brad Thompson <bradleyglind@gmail.com>
2016-11-13 20:43:22 -0700

    Click to continue / inactive

--- src/pages/Three.vue
+++ src/pages/Three.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="three">
-    <div id="three_demo" v-on:click="LockMouse()"></div>
-    {{$root.input.mouseLock}}
-    {{$root.input.rotation}}
+  <div class="three" v-bind:class="{inactive: !$root.input.mouseLock}">
+    <div class="click_to_continue" v-on:click="LockMouse()"><div class="center">Click here to continue...</div></div>
+    <div id="three_demo"></div>
   </div>
 </template>
 
@@ -118,6 +117,43 @@
   @import "../sass/mixins";
 
   .three {
+    #three_demo {
+      // render window styles
+    }
+    .click_to_continue {
+      display: none;
+    }
+  }
 
+  /* Pointer is not locked */
+  .inactive {
+    #three_demo {
+      filter: blur(5px) sepia(100%);
+    }
+    .click_to_continue {
+      display: block;
+      position: absolute;
+      z-index: 1000;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: rgba(0, 0, 0, 0.5);
+      filter: blur(0.1vh);
+      text-align: center;
+      font-size: 4vh;
+
+      div {
+        pointer-events: none;
+      }
+      .center {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        -webkit-transform: translate(-50%, -50%);
+        -moz-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+      }
+    }
   }
 </style>
